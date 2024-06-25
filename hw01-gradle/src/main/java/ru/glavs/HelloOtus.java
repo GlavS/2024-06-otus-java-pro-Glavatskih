605383991426b0a8699ef709564e739c338ad4d1
GlavS <pianoforte2@yandex.ru>
2024-06-25 19:55:19 +0300

    hw1 ready

---
+++ hw01-gradle/src/main/java/ru/glavs/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.glavs;
+
+import com.google.common.math.IntMath;
+
+public class HelloOtus {
+
+    public long guavaFactorial(int num){
+        return IntMath.factorial(num);
+    }
+}
--- hw01-gradle/src/main/java/ru/glavs/Main.java
+++ hw01-gradle/src/main/java/ru/glavs/Main.java
@@ -2,6 +2,6 @@ package ru.glavs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(new HelloOtus().guavaFactorial(8));
     }
 }
