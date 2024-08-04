f1a150a55aba71ceb43cb94f2fbbf5a657d7c2c9
GlavS <pianoforte2@yandex.ru>
2024-08-04 16:53:38 +0300

    Test results added

--- hw04-gc/bash/test.sh
+++ hw04-gc/bash/test.sh
@@ -2,10 +2,10 @@
 
 i=256
 
-while [[ $i -lt 4097 ]]
+while [[ $i -le 4096 ]]
 do
-params="-Xms${i}m -Xmx${i}m -XX:+UseSerialGC -XX:+PrintCommandLineFlags"
-java -jar $params ../build/libs/hw04gc-0.1.jar
+params="-Xms${i}m -Xmx${i}m -XX:+UseSerialGC"
+java -jar $params -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=15,filesize=10m ../build/libs/hw04gc-0.1.jar | sed -n '$p'
 printf "%s\n\n" "$params"
 ((i=i+256))
 done
