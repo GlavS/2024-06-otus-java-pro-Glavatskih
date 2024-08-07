f47e169c76104ba3d6b94e90c113f1f898c8c084
GlavS <pianoforte2@yandex.ru>
2024-08-07 22:22:02 +0300

    Readme updated

--- hw04-gc/bash/hello.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in *.log
-do
-  tail -n1 "$file" | perl script.pl
-done
--- hw04-gc/src/main/java/ru/calculator/optimized/CalcDemo.java
+++ hw04-gc/src/main/java/ru/calculator/optimized/CalcDemo.java
@@ -1,4 +1,4 @@
-package ru.calculator;
+package ru.calculator.optimized;
 
 /*
 -Xms256m
@@ -21,8 +21,10 @@ public class CalcDemo {
         var summator = new Summator();
         long startTime = System.currentTimeMillis();
 
+        var data = new Data(0);
+
         for (var idx = 0; idx < counter; idx++) {
-            var data = new Data(idx);
+            data.setValue(idx);
             summator.calc(data);
 
             if (idx % 10_000_000 == 0) {
