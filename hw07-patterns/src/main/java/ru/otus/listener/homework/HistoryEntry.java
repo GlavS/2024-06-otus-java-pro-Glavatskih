5134e3eae9404bd0498fd1cd1c86bae785e47ad6
GlavS <pianoforte2@yandex.ru>
2024-08-30 18:02:36 +0300

    History Listener implemented

--- hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryEntry.java
+++ hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryEntry.java
@@ -1,2 +1,6 @@
-package ru.otus.listener.homework;public record HistoryEntry() {
-}
+package ru.otus.listener.homework;
+
+import java.time.LocalDateTime;
+import ru.otus.model.Message;
+
+public record HistoryEntry(Message message, LocalDateTime time) {}
--- hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -1,18 +1,33 @@
 package ru.otus.listener.homework;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
 
 public class HistoryListener implements Listener, HistoryReader {
+    private final List<HistoryEntry> history = new ArrayList<>();
 
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        HistoryEntry entry = new HistoryEntry(deepCopy(msg), LocalDateTime.now());
+        history.add(entry);
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return history.stream()
+                .map(HistoryEntry::message)
+                .filter(m -> m.getId() == id)
+                .findFirst();
+    }
+
+    private static Message deepCopy(Message msg) {
+        ObjectForMessage newOfm = new ObjectForMessage();
+        newOfm.setData(List.copyOf(msg.getField13().getData()));
+        return msg.toBuilder().field13(newOfm).build();
     }
 }
