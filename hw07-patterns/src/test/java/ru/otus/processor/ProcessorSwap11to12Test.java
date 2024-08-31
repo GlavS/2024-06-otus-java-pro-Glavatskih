f2b9bdc685cd5c4645a8b802a263111eec8409a5
GlavS <pianoforte2@yandex.ru>
2024-08-31 14:51:32 +0300

    DisplayName annotation added

--- hw07-patterns/src/test/java/ru/otus/processor/ProcessorSwap11to12Test.java
+++ hw07-patterns/src/test/java/ru/otus/processor/ProcessorSwap11to12Test.java
@@ -2,12 +2,14 @@ package ru.otus.processor;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.model.Message;
 import ru.otus.processor.homework.ProcessorSwap11to12;
 
 class ProcessorSwap11to12Test {
     @Test
+    @DisplayName("Процессор должен менять местами значения 11 и 12 полей")
     void swapTest() {
         ProcessorSwap11to12 processorSwap11to12 = new ProcessorSwap11to12();
         Message message =
