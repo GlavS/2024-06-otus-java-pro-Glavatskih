8aeaa8bf20f0c3de0ad0c71e92f1a8eaeffa3aec
GlavS <pianoforte2@yandex.ru>
2024-09-02 22:52:59 +0300

    Parametrized test fix

--- hw07-patterns/src/test/java/ru/otus/processor/ProcessorExceptionEvenSecondTest.java
+++ hw07-patterns/src/test/java/ru/otus/processor/ProcessorExceptionEvenSecondTest.java
@@ -6,8 +6,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import ru.otus.model.Message;
 import ru.otus.processor.homework.EvenSecondException;
@@ -30,20 +33,31 @@ class ProcessorExceptionEvenSecondTest {
         assertThatThrownBy(() -> processor.process(emptyMsg)).isInstanceOf(EvenSecondException.class);
     }
 
-    @Test
+    @ParameterizedTest(name = "Процессор должен бросить исключение на {0} секунде")
+    @MethodSource("evenSecondsGenerator")
     @DisplayName("Процессор должен бросать заданное исключение только будучи вызванным в четную секунду")
-    void processorShouldThrowExceptionIfCalledOnEvenSecond() {
-        for (int i = 0; i < 60; i += 2) {
-            // when
-            Mockito.when(timeProviderMock.getTime()).thenReturn(LocalDateTime.of(date, LocalTime.of(1, 1, i)));
-            // then
-            assertThatThrownBy(() -> processor.process(emptyMsg)).isInstanceOf(EvenSecondException.class);
-        }
-        for (int i = 1; i < 60; i += 2) {
-            // when
-            Mockito.when(timeProviderMock.getTime()).thenReturn(LocalDateTime.of(date, LocalTime.of(1, 1, i)));
-            // then
-            assertThatNoException().isThrownBy(() -> processor.process(emptyMsg));
-        }
+    void processorShouldThrowExceptionIfCalledOnEvenSecond(int second) {
+        // when
+        Mockito.when(timeProviderMock.getTime()).thenReturn(LocalDateTime.of(date, LocalTime.of(1, 1, second)));
+        // then
+        assertThatThrownBy(() -> processor.process(emptyMsg)).isInstanceOf(EvenSecondException.class);
+    }
+
+    @ParameterizedTest(name = "Процессор не должен бросать исключение на {0} секунде")
+    @MethodSource("oddSecondsGenerator")
+    @DisplayName("Процессор не должен бросать исключение в нечетную секунду")
+    void processorShouldNotThrowExceptionIfCalledOnOddSecond(int second) {
+        // when
+        Mockito.when(timeProviderMock.getTime()).thenReturn(LocalDateTime.of(date, LocalTime.of(1, 1, second)));
+        // then
+        assertThatNoException().isThrownBy(() -> processor.process(emptyMsg));
+    }
+
+    private static IntStream evenSecondsGenerator() {
+        return IntStream.range(0, 60).filter(i -> i % 2 == 0);
+    }
+
+    private static IntStream oddSecondsGenerator() {
+        return IntStream.range(0, 60).filter(i -> i % 2 == 1);
     }
 }
