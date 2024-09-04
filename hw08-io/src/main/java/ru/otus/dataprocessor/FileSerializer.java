b7ea6ed482050939e0b5cc7a264a1b61ba5fec8e
GlavS <pianoforte2@yandex.ru>
2024-09-05 00:24:34 +0300

    Serializer done

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,30 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final ObjectMapper objectMapper;
+    private final String fileName;
 
-    public FileSerializer(String fileName) {}
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+        this.objectMapper = JsonMapper.builder()
+                .configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+                .build();
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+        File file = new File(fileName);
+        try {
+            objectMapper.writeValue(file, data);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
