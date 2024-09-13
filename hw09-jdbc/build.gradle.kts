f1cce10147259ab85e28f4f40e208870f4d0a502
GlavS <pianoforte2@yandex.ru>
2024-09-13 17:53:12 +0300

    Readme update, cleanup

--- hw09-jdbc/build.gradle.kts
+++ hw09-jdbc/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
 
     implementation("ch.qos.logback:logback-classic")
-    implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.postgresql:postgresql")
     implementation("com.zaxxer:HikariCP")
