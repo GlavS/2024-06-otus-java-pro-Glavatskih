accd0b75648ae183d78d2cdc3d2bf33f2a910dc8
GlavS <pianoforte2@yandex.ru>
2024-09-26 22:37:36 +0300

    hw11 initial setup

---
+++ hw11-cache/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,91 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public final class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.PERSIST, mappedBy = "client")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Phone> phones;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+
+        if (this.address != null) {
+            this.address.setClient(this);
+        }
+        if (this.phones != null) {
+            this.phones.forEach(phone -> phone.setClient(this));
+        }
+    }
+
+    @Override
+    @SuppressWarnings({"java:S2975"})
+    public Client clone() {
+        final Client clonedClient;
+        try {
+            clonedClient = (Client) super.clone();
+            clonedClient.setId(id);
+            clonedClient.setName(name);
+
+            List<Phone> clonedPhones = new ArrayList<>();
+            if (phones != null) {
+                phones.forEach(phone -> {
+                    Phone clonedPhone = phone.clone();
+                    clonedPhone.setClient(clonedClient);
+                    clonedPhones.add(clonedPhone);
+                });
+                    clonedClient.setPhones(clonedPhones);
+            }
+
+            if (address != null) {
+                Address clonedAddress = address.clone();
+                clonedAddress.setClient(clonedClient);
+                clonedClient.setAddress(clonedAddress);
+            }
+        } catch (CloneNotSupportedException e) {
+            throw new UnsupportedOperationException(e);
+        }
+        return clonedClient;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ include("hw07-patterns")
 include("hw08-io")
 include("hw09-jdbc")
 include("hw10-hibernate")
+include("hw11-cache")
 
 pluginManagement {
     val jgitver: String by settings
