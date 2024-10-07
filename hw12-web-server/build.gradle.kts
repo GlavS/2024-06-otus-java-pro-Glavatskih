ec1dee8c8a7b7c6346c9f06df1ca340e3d512880
GlavS <pianoforte2@yandex.ru>
2024-10-07 20:52:29 +0300

    hw12 done

--- hw12-web-server/build.gradle.kts
+++ hw12-web-server/build.gradle.kts
@@ -1,3 +1,5 @@
+val freemarker: String = "2.3.33"
+
 dependencies {
     implementation ("org.projectlombok:lombok")
     annotationProcessor ("org.projectlombok:lombok")
@@ -6,6 +8,15 @@ dependencies {
     implementation("org.hibernate.orm:hibernate-core")
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("org.flywaydb:flyway-database-postgresql")
+
+    implementation("org.eclipse.jetty.ee10:jetty-ee10-servlet")
+    implementation("org.eclipse.jetty:jetty-server")
+    implementation("org.eclipse.jetty:jetty-http")
+    implementation("org.eclipse.jetty:jetty-io")
+    implementation("org.eclipse.jetty:jetty-util")
+    implementation("org.freemarker:freemarker:$freemarker")
+
+    implementation("com.google.code.gson:gson")
 
     implementation("org.postgresql:postgresql")
     testImplementation("com.h2database:h2")
--- hw12-web-server/src/main/java/ru/otus/cache/SimpleCacheListener.java
+++
@@ -1,5 +0,0 @@
-package ru.otus.cache;
-
-public interface SimpleCacheListener<K, V> {
-    void notify(K key, V value, String action);
-}
