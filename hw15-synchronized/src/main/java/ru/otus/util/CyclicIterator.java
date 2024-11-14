df8ca1ead6189a7e725561935b3f3a7cf5f46291
GlavS <pianoforte2@yandex.ru>
2024-11-14 21:41:52 +0300

    Unneeded AtomicInteger removed

--- hw15-synchronized/src/main/java/ru/otus/util/CyclicIterator.java
+++ hw15-synchronized/src/main/java/ru/otus/util/CyclicIterator.java
@@ -1,24 +1,21 @@
 package ru.otus.util;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class CyclicIterator {
-    private final AtomicInteger count = new AtomicInteger(0);
+    private int counter;
     private boolean ascending = true;
 
     public int next() {
-        int result;
         if (ascending) {
-            result = count.incrementAndGet();
-            if (result == 10) {
+            counter++;
+            if (counter == 10) {
                 ascending = false;
             }
         } else {
-            result = count.decrementAndGet();
-            if (result == 1) {
+            counter--;
+            if (counter == 1) {
                 ascending = true;
             }
         }
-        return result;
+        return counter;
     }
 }
