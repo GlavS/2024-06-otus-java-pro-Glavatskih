fb5c1bad814d3579c8f99863c3cb18c09f0b4ad5
GlavS <pianoforte2@yandex.ru>
2024-11-23 21:44:02 +0300

    Skeleton built

--- hw17-grpc/src/main/java/ru/otus/Main.java
+++ hw17-grpc/src/main/java/ru/otus/Main.java
@@ -1,8 +1,17 @@
 package ru.otus;
 
-@SuppressWarnings("squid:S106")
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SampleMessage message = SampleMessage.newBuilder()
+                .setName("Pushkin")
+                .setAge(37)
+                .setMale(true)
+                .build();
+        logger.info("Sample message : \n{}", message);
     }
 }
